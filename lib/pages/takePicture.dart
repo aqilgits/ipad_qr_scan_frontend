b0a1b9bb1d5626c5cb5936ca8ebcd7f8338be91d
XiaoZik <81950435+XiaoZik@users.noreply.github.com>
2023-07-13 17:48:50 +0800

    intergrate camera api
    test

--- lib/pages/takePicture.dart
+++ lib/pages/takePicture.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
 class TakePicture extends StatefulWidget {
   const TakePicture({super.key});
@@ -8,6 +10,26 @@ class TakePicture extends StatefulWidget {
 }
 
 class _TakePictureState extends State<TakePicture> {
+  @override
+  void initState() {
+    super.initState();
+    // ignore: avoid_print
+    pickImage();
+  }
+
+  File? image;
+  Future pickImage() async {
+    print('function invoked');
+    try {
+      final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+      if (image == null) return;
+      final imageTemp = File(image.path);
+      setState(() => this.image = imageTemp);
+    } on Exception catch (e) {
+      print('Failed to pick image: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
