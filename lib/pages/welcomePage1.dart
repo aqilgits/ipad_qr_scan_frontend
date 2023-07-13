df1d02792a20dc79e3f16c84dd59bc2a5fc4d4e9
XiaoZik <81950435+XiaoZik@users.noreply.github.com>
2023-07-13 21:01:06 +0800

    latest welcomepage commit
    latest welcomepage commit

--- lib/pages/welcomePage1.dart
+++ lib/pages/welcomePage1.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class WelcomePage1 extends StatefulWidget {
   const WelcomePage1({Key? key, required this.code}) : super(key: key);
@@ -11,6 +12,52 @@ class WelcomePage1 extends StatefulWidget {
 class _WelcomePage1State extends State<WelcomePage1> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Text(widget.code));
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    final Widget svgPetronas =
+        SvgPicture.asset('assets/petronas-logo-white.svg', width: width * .16);
+    final ButtonStyle style = ElevatedButton.styleFrom(
+      backgroundColor: Color(0XFF00A19C),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+    );
+    return Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          backgroundColor: Color(0XFF222222),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              ElevatedButton(
+                style: style,
+                onPressed: () {},
+                child: Center(
+                  child: Row(
+                    children: [
+                      Icon(
+                        // <-- Icon
+                        Icons.arrow_back,
+                        size: width * .02,
+                        color: Colors.white,
+                      ),
+                      SizedBox(width: width * .01),
+                      Text(
+                        'Go Back',
+                        style:
+                            TextStyle(color: Colors.white, fontSize: width * .02),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8),
+                child: Align(child: svgPetronas),
+              ),
+            ],
+          ),
+        ),
+        body: Text(widget.code));
   }
 }
