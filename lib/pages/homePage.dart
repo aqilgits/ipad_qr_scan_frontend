90a5cd18c8ee134e3d6cde03d41904ca15d73beb
XiaoZik <81950435+XiaoZik@users.noreply.github.com>
2023-07-16 09:41:00 +0800

    update design of homepage
    updated

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -4,9 +4,36 @@ import 'package:intl/intl.dart';
 import 'package:ipad_qr_scan_frontend/apis/cameraApi.dart';
 import 'package:ipad_qr_scan_frontend/apis/qrScanner.dart';
 import 'package:ipad_qr_scan_frontend/pages/previewPage.dart';
+import 'package:video_player/video_player.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  VideoPlayerController? videoPlayerController;
+  // ChewieController? chewieController;
+
+  @override
+  void initState() {
+    super.initState();
+    videoPlayerController =
+        VideoPlayerController.asset("assets/kiosk-bg-video-vertical.mp4")
+          ..initialize().then((_) {
+            videoPlayerController!.play();
+            videoPlayerController!.setLooping(true);
+            setState(() {});
+          });
+  }
+
+  @override
+  void dispose() {
+    videoPlayerController!.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,148 +44,168 @@ class HomePage extends StatelessWidget {
     final Widget userQR =
         SvgPicture.asset('assets/visitor-register.svg', width: width * .13);
     final Widget svgPetronas =
-        SvgPicture.asset('assets/petronas-logo-dark.svg', width: width * .2);
+        SvgPicture.asset('assets/petronas-logo-white.svg', width: width * .2);
     final now = DateTime.now();
     String formattedDate = DateFormat.MMMMEEEEd().format(now);
     String formattedTime = DateFormat.jm().format(DateTime.now());
+
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.only(top: height * .025),
-        child: Center(
-            child: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.only(left: width * .025),
-              child: Align(
-                  alignment: Alignment.topLeft,
-                  child: Text(
-                    formattedTime,
-                    style: TextStyle(fontSize: width * .05),
-                  )),
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: width * .025),
-              child: Align(
-                  alignment: Alignment.topLeft,
-                  child: Text(formattedDate,
-                      style: TextStyle(fontSize: width * .025))),
-            ),
-            SizedBox(
-              height: height * .04,
-            ),
-            Text(
-              'Welcome to',
-              style: TextStyle(
-                  color: const Color(0XFF101828).withOpacity(0.5),
-                  fontSize: width * .025),
-            ),
-            Text(
-              'Petronas Twin Towers',
-              style: TextStyle(
-                  color: Color(0XFF101828),
-                  fontSize: width * .05,
-                  fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: height * .05),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+      body: Stack(
+        children: [
+          Expanded(child: VideoPlayer(videoPlayerController!)),
+          Padding(
+            padding: EdgeInsets.only(top: height * .025),
+            child: Center(
+                child: Column(
               children: [
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => QRScanner(screenClosed: false)),
-                    );
-                  },
-                  child: Card(
-                    color: Color(0xFF00A19C),
-                    child: SizedBox(
-                      width: width * .3,
-                      height: height * .2,
-                      child: Center(
-                        child: Column(
-                          children: [
-                            SizedBox(height: height * .025),
-                            Align(
-                                alignment: const Alignment(0.1, 0.6),
-                                child: svgQR),
-                            SizedBox(
-                              height: height * .015,
-                            ),
-                            Text(
-                              'Pre-registered visitors',
-                              style: TextStyle(
-                                  color: Colors.white.withOpacity(0.8),
-                                  fontSize: width * .02),
-                            ),
-                            Text('Scan QR',
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: width * .03)),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
+                Padding(
+                  padding: EdgeInsets.only(left: width * .025),
+                  child: Align(
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                        formattedTime,
+                        style: TextStyle(fontSize: width * .05, color: const Color(0XFFFFFFFF)),
+                      )),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: width * .025),
+                  child: Align(
+                      alignment: Alignment.topLeft,
+                      child: Text(formattedDate,
+                          style: TextStyle(fontSize: width * .025, color: const Color(0XFFFFFFFF)))),
                 ),
                 SizedBox(
-                  width: width * .04,
+                  height: height * .04,
                 ),
-                GestureDetector(
-                  onTap: () async {
-                    final data = await pickImage();
-                    if (data != null) {
-                      print(data);
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: ((context) => PreviewPage(
-                                imageFile: data,
-                              )),
-                        ),
-                      );
-                    }
-                  },
-                  child: Card(
-                    color: Color(0xFF763F98),
-                    child: SizedBox(
-                      width: width * .3,
-                      height: height * .2,
-                      child: Center(
-                        child: Column(
-                          children: [
-                            SizedBox(height: height * .025),
-                            userQR,
-                            SizedBox(
-                              height: height * .015,
+                Text(
+                  'WELCOME TO',
+                  style: TextStyle(
+                      color: const Color(0XFFFFFFFF).withOpacity(0.8),
+                      fontSize: width * .025),
+                ),
+                Text(
+                  'Petronas Twin Towers',
+                  style: TextStyle(
+                      color: Color(0XFFFFFFFF),
+                      fontSize: width * .05,),
+                ),
+                SizedBox(height: height * .05),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) =>
+                                  QRScanner(screenClosed: false)),
+                        );
+                      },
+                      child: Card(
+                        color: Color(0xFF00A19C),
+                        child: SizedBox(
+                          width: width * .3,
+                          height: height * .2,
+                          child: Center(
+                            child: Column(
+                              children: [
+                                SizedBox(height: height * .025),
+                                Align(
+                                    alignment: const Alignment(0.1, 0.6),
+                                    child: svgQR),
+                                SizedBox(
+                                  height: height * .015,
+                                ),
+                                Text(
+                                  'Pre-registered visitors',
+                                  style: TextStyle(
+                                      color: Colors.white.withOpacity(0.8),
+                                      fontSize: width * .02),
+                                ),
+                                Text('Scan QR',
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: width * .03)),
+                              ],
                             ),
-                            Text('All visitors',
-                                style: TextStyle(
-                                    color: Colors.white.withOpacity(0.8),
-                                    fontSize: width * .02)),
-                            Text('Register Here',
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: width * .03))
-                          ],
+                          ),
                         ),
                       ),
                     ),
-                  ),
+                    SizedBox(
+                      width: width * .04,
+                    ),
+                    GestureDetector(
+                      onTap: () async {
+                        final data = await pickImage();
+                        if (data != null) {
+                          print(data);
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: ((context) => PreviewPage(
+                                    imageFile: data,
+                                  )),
+                            ),
+                          );
+                        }
+                      },
+                      child: Card(
+                        color: Color(0xFF763F98),
+                        child: SizedBox(
+                          width: width * .3,
+                          height: height * .2,
+                          child: Center(
+                            child: Column(
+                              children: [
+                                SizedBox(height: height * .025),
+                                userQR,
+                                SizedBox(
+                                  height: height * .015,
+                                ),
+                                Text('All visitors',
+                                    style: TextStyle(
+                                        color: Colors.white.withOpacity(0.8),
+                                        fontSize: width * .02)),
+                                Text('Register Here',
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: width * .03))
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
+                SizedBox(height: height * .47),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      svgPetronas,
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Text('Waiting time saved : ', style: TextStyle(color: const Color(0XFFFFFFFF)
+                          ),),
+                          Text('302,480 Minutes',
+                              style: TextStyle(fontFamily: 'Vcr',
+                                  fontSize: width * .03, color: const Color(0XFFFFFFFF))),
+                        ],
+                      ),
+                    ],
+                  ),
+                )
               ],
-            ),
-            SizedBox(height: height * .15),
-            svgPetronas,
-            SizedBox(height: height * .025),
-            Text('302,480 hours of waiting time saved!',
-                style: TextStyle(
-                    fontSize: width * .03, fontWeight: FontWeight.bold))
-          ],
-        )),
+            )),
+          ),
+        ],
       ),
     );
   }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,10 +9,14 @@ import file_selector_macos
 import firebase_core
 import firebase_storage
 import mobile_scanner
+import package_info_plus
+import wakelock_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
+  FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
+  WakelockPlusMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockPlusMacosPlugin"))
 }
